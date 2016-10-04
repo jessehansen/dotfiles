94cfcc089a9552834ea1a25d74e80d75b48b081e
Jesse Hansen <twindagger@gmail.com>
2016-10-03 20:53:25 -0600

    Removing auth tokens

--- exports.zsh
+++ exports.zsh
@@ -4,8 +4,8 @@ export EDITOR='sublime -w'
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 export BUILDER_ID_RSA=~/.ssh/id_rsa
-export NPM_AUTH_TOKEN=61185b91-d323-433a-8eaf-85100b6e8f80
-export HOMEBREW_GITHUB_API_TOKEN=5290188b108d864215f31eb257f601c9b18c7984
+export NPM_AUTH_TOKEN=
+export HOMEBREW_GITHUB_API_TOKEN=
 
 export RABBITMQ_HOST=docker.dev
 export MONGO_HOST=docker.dev
