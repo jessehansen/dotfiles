5a90bc26d712fd693f21d53d2bb4fbfaab94c946
Jesse Hansen <twindagger@gmail.com>
2019-12-29 10:36:52 -0700

    Only add go and yarn to PATH if they are available

--- exports.zsh
+++ exports.zsh
@@ -4,8 +4,13 @@ export EDITOR='nvim'
 
 export BUILDER_ID_RSA=~/.ssh/id_rsa
 export NPM_AUTH_TOKEN=
-export GOPATH="/Users/jesse.hansen/go"
-export PATH=$PATH:$(go env GOPATH)/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin
+if (( $+commands[go] )); then
+	export GOPATH="/Users/jesse.hansen/go"
+	export PATH=$PATH:$(go env GOPATH)/bin
+fi
+if (( $+commands[yarn] )); then
+	export PATH=$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin
+fi
 export LESS="-FRX"
 export KEYTIMEOUT=1
 #ripgreprc
