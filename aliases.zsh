879c7560b44f431c91f2b9f0c166e55fdccf89d8
Jesse Hansen <jesse.hansen@ooda-health.com>
2021-04-27 11:07:59 -0600

    Update iterm profile

--- aliases.zsh
+++ aliases.zsh
@@ -32,6 +32,7 @@ alias gcan="git commit -a --amend --no-edit --date=now"
 alias gst="git status -s"
 alias gpb="git prune-branches-ok"
 alias hstat="curl -o /dev/null --silent --head --write-out '%{http_code}\n'" $1
+alias gs="git status -s"
 
 # master -> main
 alias gcm="git checkout main"
