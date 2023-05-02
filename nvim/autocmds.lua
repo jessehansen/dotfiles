6898d81bedde6ddbdb1f7ad0892361c42eaaeaf5
Jesse Hansen <twindagger@gmail.com>
2023-05-02 13:39:08 -0600

    Config sync

--- nvim/autocmds.lua
+++ nvim/autocmds.lua
@@ -5,6 +5,17 @@ vim.api.nvim_create_autocmd('Filetype', { pattern = 'help', command = 'wincmd L'
 -- :bp and :bn should skip quickfix buffers
 vim.api.nvim_create_autocmd('FileType', { pattern = 'qf', command = 'set nobuflisted', group = 'dotfiles' })
 
+-- Spell options for typescript & typescript react
+-- Some plugin is overriding & removing camel, so fix it on buf enter
+vim.api.nvim_create_autocmd(
+  'BufEnter',
+  { pattern = '*.tsx', command = 'set spelloptions=camel,noplainbuffer', group = 'dotfiles' }
+)
+vim.api.nvim_create_autocmd(
+  'BufEnter',
+  { pattern = '*.ts', command = 'set spelloptions=camel,noplainbuffer', group = 'dotfiles' }
+)
+
 -- Highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
