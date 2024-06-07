ddb91be3a100ca68a89397df3092e466d7b06437
Jesse Hansen <twindagger@gmail.com>
2024-06-07 14:14:32 -0600

    Add <c-j> mapping for auto-suggest in zsh

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -3,4 +3,5 @@ bindkey '^K' kill-whole-line            # ctrl-k
 bindkey '^[[3~' delete-char
 bindkey -a '^[[3~' delete-char
 bindkey '^ ' autosuggest-accept         # ctrl+<space>
+bindkey '^j' autosuggest-accept         # ctrl+j (because I'm used to copilot mapping)
 bindkey '^[[[CE' autosuggest-execute    # ctrl+<enter> (remapped with iterm)
